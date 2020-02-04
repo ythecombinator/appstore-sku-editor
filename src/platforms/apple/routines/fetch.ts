0dff3d9cf979178e990a90deb4d46159d9d13254
Matheus Albuquerque <land@ythecombinator.space>
2020-02-04 04:49:23 +0100

    feat(platforms/apple/routines/fetch): iterate through IAPs and pretty print

--- src/platforms/apple/routines/fetch.ts
+++ src/platforms/apple/routines/fetch.ts
@@ -6,17 +6,48 @@ import { navigateToMyApps } from '../tasks/navigate-to-my-apps';
 import { navigateToIAPs } from '../tasks/navigate-to-iap';
 import { parseIAPs } from '../tasks/parse-iap';
 import { fetchIAPdata } from '../tasks/fetch-iap-data';
+import { MappedInAppPurchasePricing } from '../models/InAppPurchase';
+
+import { logger } from '../../../util/terminal';
+import { meter } from '../../../util/performance';
 
 const fetchData = async (page: Page, config: AppStoreConnectConfig) => {
   const { app, credentials } = config;
 
-  await login(page, credentials);
-  await navigateToMyApps(page);
-  await navigateToIAPs(page, app.id);
-  const items = await parseIAPs(page);
-  const data = await fetchIAPdata(page, items[0]);
+  meter.start();
 
-  console.log(data);
+  // Signing In
+  logger.init('Signing in');
+  await login(page, credentials);
+  logger.finish('Signing in');
+
+  // Navigating to apps list
+  logger.init('Navigating to apps list');
+  await navigateToMyApps(page);
+  logger.finish('Navigating to apps list');
+
+  // Navigating to IAPs list
+  logger.init('Navigating to IAPs list');
+  await navigateToIAPs(page, app.id);
+  logger.finish('Navigating to IAPs list');
+
+  // Parsing IAPs list
+  logger.init('Parsing IAPs list');
+  const items = await parseIAPs(page);
+  logger.finish('Parsing IAPs list');
+
+  // Iterating results
+  const results = [] as MappedInAppPurchasePricing[][];
+
+  for (const item of items) {
+    logger.init(`Fetching item: ${item.name}`);
+    const result = await fetchIAPdata(page, item);
+    results.push(result);
+    logger.finish(`Fetching item: ${item.name}`);
+  }
+
+  const { time } = meter.stop();
+  logger.info(`Fetched ${results.length} items in ${time}s. 🤓`);
 };
 
 export { fetchData };
